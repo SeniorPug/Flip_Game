1d869f0728e186c9ea025266f9fbdf2f4ca9ff7c
SeniorPug <41790789+SeniorPug@users.noreply.github.com>
2020-11-03 19:13:00 +0200

    Add files via upload

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour
+{
+    public int cardID = 0;
+    public int imgID = 0;
+    bool flipped = false;
+    public GameObject back;
+    public RawImage rImage;
+    Animator anim;
+    string url = "";
+
+    public void FlipTrigger()
+    {
+        if (flipped)
+        {
+            back.SetActive(true);
+            flipped = false;
+            GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            back.SetActive(false);
+            flipped = true;
+            GameScript.flipped_cards++;
+            StartCoroutine(CardCheck());
+        }
+    }
+
+    public void Flip()
+    {
+        GetComponent<Button>().interactable = false;
+        if (!flipped && GameScript.flipped_cards < 2)
+        {
+            anim.Play("Flip");
+        }
+    }
+
+    void Start()
+    {
+        GetComponent<Button>().interactable = false;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (GameScript.fresh_game)
+        {
+            StartCoroutine(IdleTrigger());
+        }
+    }
+
+    IEnumerator CardCheck()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        if (GameScript.cur_ID == null)
+        {
+            GameScript.cur_ID = gameObject;
+        }
+        else if (GameScript.cur_ID.GetComponent<CardScript>().imgID == imgID)
+        {
+            GameScript.cur_ID.GetComponent<Animator>().Play("Destroy");
+            GameScript.cardsArray[GameScript.cur_ID.GetComponent<CardScript>().cardID - 1] = 0;
+            GameScript.cardsArray[cardID - 1] = 0;
+            GameScript.flipped_cards = 0;
+            GameScript.cur_ID = null;
+            anim.Play("Destroy");
+        }
+        else
+        {
+            GameScript.cur_ID.GetComponent<Animator>().Play("Flip");
+            anim.Play("Flip");
+            GameScript.cur_ID = null;
+            GameScript.flipped_cards = 0;
+        }
+    }
+    IEnumerator IdleTrigger()
+    {
+        imgID = GameScript.cardsArray[cardID - 1];
+        url = GameScript.img.array[GameScript.theme_id] + (imgID + 1).ToString() + ".png";
+        StartCoroutine(LoadImage());
+        yield return new WaitForSecondsRealtime(1f);
+        back.SetActive(true);
+        flipped = false;
+        yield return new WaitForEndOfFrame();
+        anim.Play("Idle");
+        GameScript.fresh_game = false;
+        GetComponent<Button>().interactable = true;
+    }
+
+    IEnumerator LoadImage()
+    {
+        rImage.texture = null;
+        WWW wwwLoader = new WWW(url);
+        yield return wwwLoader;
+        rImage.texture = wwwLoader.texture;
+    }
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    public static GameObject cur_ID;
+    public static int[] cardsArray = new int[6] { 0, 0, 0, 0, 0, 0 };
+    public static int theme_id;
+    public static int flipped_cards = 0;
+    public static bool fresh_game = true;
+    public class imgArray
+    {
+        public List<string> array { get; set; }
+    }
+
+    public static imgArray img;
+
+    void CardArrayGenerator()
+    {
+        fresh_game = true;
+        theme_id = Random.Range(0, img.array.Count);
+        int img_num = 0;
+        switch (theme_id)
+        {
+            case 0:
+                img_num = 19;
+                break;
+            case 1:
+                img_num = 7;
+                break;
+            default:
+                break;
+        }
+        for (int i = 0; i < cardsArray.Length; i++)
+        {
+            if (cardsArray[i] == 0)
+            {
+                int rand_num = Random.Range(i, img_num);
+                if (!cardsArray.Contains(rand_num))
+                {
+                    cardsArray[i] = rand_num;
+                    bool check = false;
+                    do
+                    {
+                        rand_num = Random.Range(i + 1, 6);
+                        if (cardsArray[rand_num] == 0)
+                        {
+                            cardsArray[rand_num] = cardsArray[i];
+                            check = true;
+                        }
+                    } while (!check);
+                } else
+                {
+                    i--;
+                }
+            }
+        }
+    }
+
+    void Start()
+    {
+        using (StreamReader reader = new StreamReader("Assets/Images.json"))
+        {
+            string json = reader.ReadToEnd();
+            img = JsonConvert.DeserializeObject<imgArray>(json);
+        }
+        CardArrayGenerator();
+    }
+
+    void Update()
+    {
+        if (cardsArray[0] == 0 && cardsArray[1] == 0 && cardsArray[2] == 0 && cardsArray[3] == 0 && cardsArray[4] == 0 && cardsArray[5] == 0)
+        {
+            CardArrayGenerator();
+        }
+    }
+}
